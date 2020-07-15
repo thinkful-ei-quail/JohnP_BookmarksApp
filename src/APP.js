b183efdb706d435a580daeb96b0fe2ae32573b0c
John Pendergast <jpending@gmail.com>
2020-07-15 12:53:31 -0400

    continuing work on wireframe and begin generator funcs

--- src/APP.js
+++ src/APP.js
@@ -7,20 +7,89 @@
 
 ///////////////////////////////////MU GENERATOR FUNCS//
 
-//generate(){}:
+//const generate = function (){};
+
+const generateStart = function (){
+    $('main').html(`
+    <section class="display-container">
+    <section><div class="add-new"><button class="start-add-bookmark" type="submit"></button></div><div class="filter-div"></div></section>
+    <div class="list-display">
+      <ul class="list-ul"></ul>
+    </div>
+  </section>
+      `)
+};
+
+const generateFilter = function (){
+    $('.filter-div').html(`
+    <label for="rating-select">minimum rating:</label>
+    <select class="rating-filter" name="rating-select" id="rating-select">
+      <option value="1">Bad</option>
+      <option value="2">so-so</option>
+      <option value="3">Good</option>
+      <option value="4">Great</option>
+      <option value="5">Amazing</option>
+    </select>
+
+    `)
+};
+
+const generateListItem= function (bookmarks){
+    return `
+        <li data-item-id="${bookmarks.id}">
+        <div class="small-div">
+          <div class="small-list-title">
+            <h3>${bookmarks.title}</h3><button class="expand-from-list" type="submit"></button>
+          </div>
+          <div class="list-rating">
+            <h3>${bookmarks.rating}</h3>
+          </div>
+          <div class="list-see-more"></div>
+        </div>
+      </li>
+        `
+    };
+
+    generateExpandedBook = function (){
+        return `
+    <div class="xpanded">
+        <div>
+            <div class="xpanded-title">
+                <h2>${bookmarks.title}</h2>
+            </div>
+            <div class="edit-and-delete-div">
+                <button class="edit-title" type="submit"></button>
+                <button class="delete" type="submit"></button>
+            </div>
+            <div class="xpanded-link">
+                <a href="${bookmarks.url}">Go to Site</a>
+            </div>
+            <div class="xpanded-desc">
+                <p>${bookmarks.description}</p>
+            </div>
+            <button class="edit-desc" type="submit"></button>
+            <div class="xpanded-rating">
+                <h2>${bookmarks.rating}</h2>
+            </div>
+            <button class="edit-rating" type="submit"></button>
+        </div>
+    </div>
+        `
+    };
+
 
 
 
 //////////////////Render Funcs
 
-//render(){};
+//const render = function () {};
 
 
 
 
 ///////////////////Event Handler Funcs
 
-//handle(){};
+//const handle = function (){};
 
 ///bindListener func
 
--- src/store.js
+++ src/store.js
@@ -4,10 +4,47 @@
 
 ///constants for whole app go here
 //defaults sates etc
-// const POSTS_URL = `https://jsonplaceholder.typicode.com/posts`;
-// const USERS_URL = `https://jsonplaceholder.typicode.com/users`;
 
-///funcitons for finding things ie addBook
+//const items = [];
+//let error = null;
+// const BASE_URL = `https://thinkful-list-api.herokuapp.com/johnpendergast/bookmarks`;
+
+
+const store = {
+    bookmarks: [
+      {
+        id: 'x56w',
+        title: 'Title 1',
+        rating: 3,
+        url: 'http://www.title1.com',
+        description: 'lorem ipsum dolor sit',
+        expanded: false
+      },
+      {
+        id: '6ffw',
+        title: 'Title 2',
+        rating: 5,
+        url: 'http://www.title2.com',
+        description: 'dolorum tempore deserunt',
+        expanded: false
+      }
+
+    ],
+    adding: false,
+    error: null,
+    filter: 0
+  };
+
+
+
+
+
+
+//const findById = function (id) {
+//     return this.items.find(currentItem => currentItem.id === id);
+// };
+
+///funcitons for finding things by id ie addBook
 ///func removeBook
 ///upddate ratings
 ///filterByRating
